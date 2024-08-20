692a98c00cc891e6a752ebee7a775317f5c16575
Marco Lucio <marcoalucio15@gmail.com>
2024-08-19 22:47:37 -0600

    Desplazamiento simple.

--- Api/Api_MESA.py
+++ Api/Api_MESA.py
@@ -6,13 +6,11 @@ class VehicleAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
 
+    # Al momento, el agente se mueve solamente en la dirección Y positiva, en donde se encuentra el objetivo final
     def step(self):
         x, y = self.pos
-
         print(f"Posición Actual: {self.pos}")
-
-        if x < 5:
-            self.model.grid.move_agent(self, (x + 1, y))
+        self.model.grid.move_agent(self, (x, y + 1))
 
 
 
@@ -21,19 +19,23 @@ class VehicleModel(Model):
 
         super().__init__()
 
-        self.grid = MultiGrid(6, 6, torus=False)
+        self.grid = MultiGrid(11, 11, torus=False)
         self.schedule = SimultaneousActivation(self)
 
         # Create agents
         agent = VehicleAgent(1, self)
-        self.grid.place_agent(agent, (1, 3))
+        print("Se coloca al agente en la posición inicial (5,0)")
+        self.grid.place_agent(agent, (5, 0))
         self.schedule.add(agent)
 
     def step(self):
         self.schedule.step()
 
+
+print("Inicializando sistema y colocando al agente en la posición inicial..")
 model = VehicleModel()
-for i in range(4):  # Necesita 4 pasos para llegar a (5, 3)
+print("Se desplazará al agente hacia el final de la calle a la posición (5, 10)")
+for i in range(10):
     model.step()
 
 agent_position = [agent.pos for agent in model.schedule.agents][0]
